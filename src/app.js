61515534e61c3b77328a19efce486b86e47bc8d3
Sithum Bimsara <sithim.22@cse.mrt.ac.lk>
2025-07-19 15:17:35 +0530

    Connected to backend

--- src/app.js
+++ src/app.js
@@ -1,19 +1,17 @@
-// src/app.js
 import express from 'express';
-//import userRoutes from './routes/userRoutes.js';
+import userRoutes from './routes/userRoutes.js';
 import dotenv from 'dotenv';
 
 dotenv.config();
 
 const app = express();
 
-// Middleware
-app.use(express.json()); // for parsing application/json
+app.use(express.json()); 
 
 // Routes
-//app.use('/api/users', userRoutes);
+app.use('/api/users', userRoutes);
 
-// Health check route (optional)
+
 app.get('/', (req, res) => {
   res.send('API is running...');
 });
---
+++ src/controllers/userController.js
@@ -0,0 +1,11 @@
+// src/controllers/userController.js
+import { fetchUsers } from '../services/userService.js';
+
+export const getAllUsers = async (req, res) => {
+  try {
+    const users = await fetchUsers();
+    res.status(200).json(users);
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+};
