3b88a93f406631c8e8473a7e7f393a29415dc3a4
NipunSGeeTH <sangeethnipun385@gmail.com>
2025-07-20 14:58:08 +0530

    few changes

--- src/middlewares/authMiddleware.js
+++ src/middlewares/authMiddleware.js
@@ -20,7 +20,7 @@ export const requireAuth = async (req, res, next) => {
     // Fetch role from your user table once here
     const { data: userRow, error: userError } = await supabase
       .from('user')
-      .select('user_id')
+      .select('user_id, role')
       .eq('user_id', user.id)
       .single();
 
@@ -29,6 +29,7 @@ export const requireAuth = async (req, res, next) => {
     }
 
     req.user = user;
+    req.userRole = userRow.role;
     //.userRole = userRow.role;  // Attach role to request for next middlewares
     req.userId = userRow.user_id; // Attach user_id to request for next middlewares
     //console.log(req.userRole);
--- src/routes/supperAdminRoutes.js
+++ src/routes/supperAdminRoutes.js
@@ -1,9 +1,10 @@
 import express from 'express';
 import { requireAuth } from "../middlewares/authMiddleware.js";
 import { getAllusers } from '../controllers/supperAdminController.js';
+import { requireSupperAdmin } from "../middlewares/requireSupperAdmin.js";
 //get all users data from the database
 
 
 const router = express.Router();
-router.get('/all-users',requireAuth,getAllusers)
+router.get('/all-users',requireAuth,requireSupperAdmin,getAllusers)
 export default router;
