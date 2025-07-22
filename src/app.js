7eb05e6d560ed59ae88260b41627e82853aecc5e
Sithum Bimsara <sithim.22@cse.mrt.ac.lk>
2025-07-23 01:55:47 +0530

    Checked salonRoutes

--- src/app.js
+++ src/app.js
@@ -1,7 +1,6 @@
 import express from 'express';
 import authRoutes from './routes/authRoutes.js';
 
-import protectedRoutes from './routes/protectedRoutes.js';
 import salonAdminRoutes from './routes/salonAdminRoutes.js';
 import supperAdminRoutes from './routes/supperAdminRoutes.js';
 import salonRoutes from './routes/salonRoutes.js';
@@ -17,17 +16,6 @@ app.use(express.json());
 // Routes
 app.use('/api/auth', authRoutes);
 app.use('/api/salon-admin', salonAdminRoutes);
-
-app.use('/api/salon-admin', salonAdminRoutes);
-
-
-app.use('/api/protected', protectedRoutes);
-
-
-
-app.use('/api/protected', protectedRoutes);
-
-
 app.use('/api/salons',salonRoutes);
 app.use('/api/supper-admin', supperAdminRoutes);
 
--- src/routes/salonRoutes.js
+++ src/routes/salonRoutes.js
@@ -4,7 +4,6 @@ import {
   getSalonById,
   getSalonsByLocation,
   getSalonsByName,
-  getSalonsByServiceType,
   getSalonsByType,
   getStylistsBySalonId,
   getAvailableTimeSlots
@@ -12,17 +11,22 @@ import {
 
 const router = express.Router();
 
-router.get('/', getAllSalons); // Fetch all salons
-router.get('/:id', getSalonById);   // Fetch salon by ID http://localhost:3000/api/salons/c49cf780-e2db-4a94-b1f2-bf2aa47f785b
+// Get all salons
+router.get('/', getAllSalons);
+// Get salons by ID
+router.get('/:id', getSalonById);               // http://localhost:3000/api/salons/c49cf780-e2db-4a94-b1f2-bf2aa47f785b
+// Get salons by location
 router.get('/location', getSalonsByLocation);   //not working
-router.get('/name/:name', getSalonsByName); // http://localhost:3000/api/salons/name/Sithum
-// router.get('/type/service/:serviceType', getSalonsByServiceType);
+// Get salons by name
+router.get('/name/:name', getSalonsByName);     // http://localhost:3000/api/salons/name/Sithum
+// Get salons by service type
+//router.get('/type/service/:serviceType', getSalonsByServiceType);
 
-
-router.get('/type/:id', getSalonsByType);       //http://localhost:3000/api/salons/type/2da738de-ab0e-4cc5-9e5a-a4bfcbb43dc0
-
-router.get('/:id/stylists', getStylistsBySalonId);      //http://localhost:3000/api/salons/2da738de-ab0e-4cc5-9e5a-a4bfcbb43dc0/stylists
-
+// Get salons by type unisex, male, female
+router.get('/type/:id', getSalonsByType);                               //http://localhost:3000/api/salons/type/2da738de-ab0e-4cc5-9e5a-a4bfcbb43dc0
+// Get all stylists by salon ID                                 
+router.get('/:id/stylists', getStylistsBySalonId);                      //http://localhost:3000/api/salons/2da738de-ab0e-4cc5-9e5a-a4bfcbb43dc0/stylists
+// Get available time slots for a stylist
 router.get('/stylist/:stylistId/availability', getAvailableTimeSlots);  //http://localhost:3000/api/salons/stylist/b5991531-a0e1-460e-bce9-3dee1056d3b6/availability
 
 export default router;
