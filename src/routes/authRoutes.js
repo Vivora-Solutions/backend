f7f774f321a08f52f82633e14436cda3f297e7ce
DaminduDeSilva <damiya2384@gmail.com>
2025-08-08 18:18:58 +0530

    Add customer and salon registration functionality with OAuth support

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -1,22 +1,30 @@
-import express from 'express';
+import express from "express";
 import {
-    loginUser,
-    logoutUser,
-    refreshToken,
-    getAuthenticatedUser, registerCustomerController, registerSalonController
-} from '../controllers/authController.js';
+  loginUser,
+  logoutUser,
+  refreshToken,
+  getAuthenticatedUser,
+  registerCustomerController,
+  registerSalonController,
+  completeOAuthRegistrationController,
+  registerCustomerControllerGoogle,
+} from "../controllers/authController.js";
 
-import {requireAuth} from '../middlewares/authMiddleware.js';
+import { requireAuth } from "../middlewares/authMiddleware.js";
 
 const router = express.Router();
 
-router.post('/register-customer', registerCustomerController);
-router.post('/register-salon', registerSalonController);
-router.post('/login', loginUser);
-router.post('/logout', logoutUser);
-router.get('/me', requireAuth, getAuthenticatedUser);
-router.post('/refresh-token', refreshToken);
-
-
+router.post("/register-customer", registerCustomerController);
+router.post("/register-customer-google", registerCustomerControllerGoogle);
+router.post("/register-salon", registerSalonController);
+router.post("/login", loginUser);
+router.post("/logout", logoutUser);
+router.get("/me", requireAuth, getAuthenticatedUser);
+router.post("/refresh-token", refreshToken);
+// ...existing code...
+router.post(
+  "/complete-oauth-registration",
+  completeOAuthRegistrationController
+);
 
 export default router;
