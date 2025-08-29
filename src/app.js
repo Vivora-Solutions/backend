320ea7138d2d16114c7e72eb64acb1fa5453e231
DaminduDeSilva <damiya2384@gmail.com>
2025-08-29 15:31:27 +0530

    change corsOptions

--- src/app.js
+++ src/app.js
@@ -1,12 +1,12 @@
-import express from 'express';
-import cors from 'cors';
-import cookieParser from 'cookie-parser';
-import dotenv from 'dotenv';
+import express from "express";
+import cors from "cors";
+import cookieParser from "cookie-parser";
+import dotenv from "dotenv";
 
-import authRoutes from './routes/authRoutes.js';
-import salonAdminRoutes from './routes/salonAdminRoutes.js';
-import superAdminRoutes from './routes/superAdminRoutes.js';
-import salonRoutes from './routes/salonRoutes.js';
+import authRoutes from "./routes/authRoutes.js";
+import salonAdminRoutes from "./routes/salonAdminRoutes.js";
+import superAdminRoutes from "./routes/superAdminRoutes.js";
+import salonRoutes from "./routes/salonRoutes.js";
 import userBookingRoutes from "./routes/userBookingRoutes.js";
 import userReviewRoutes from "./routes/UserReviewRoutes.js";
 import profileRoute from "./routes/profileRoute.js";
@@ -16,10 +16,9 @@ dotenv.config();
 const app = express();
 
 //  Proper CORS setup to allow cookies
- const corsOptions = {
-    origin: 'http://localhost:5173',
-    // origin: 'https://web5-swart.vercel.app',
-
+const corsOptions = {
+  // origin: 'http://localhost:5173',
+  origin: "https://web5-swart.vercel.app",
 
   credentials: true,
 };
@@ -29,18 +28,18 @@ app.use(express.json());
 app.use(cookieParser());
 
 // Routes
-app.use('/api/auth', authRoutes);
-app.use('/api/salon-admin', salonAdminRoutes);
-app.use('/api/super-admin', superAdminRoutes);
+app.use("/api/auth", authRoutes);
+app.use("/api/salon-admin", salonAdminRoutes);
+app.use("/api/super-admin", superAdminRoutes);
 
-app.use('/api/salons', salonRoutes);
-app.use('/api/review', userReviewRoutes);
-app.use('/api/bookings', userBookingRoutes);
+app.use("/api/salons", salonRoutes);
+app.use("/api/review", userReviewRoutes);
+app.use("/api/bookings", userBookingRoutes);
 
-app.use('/api/profile', profileRoute);
+app.use("/api/profile", profileRoute);
 
-app.get('/', (req, res) => {
-  res.send('API is running...');
+app.get("/", (req, res) => {
+  res.send("API is running...");
 });
 
 export default app;
